215694a86abc00f085d2cf53690c9916a223f903
CuiyingLin17 <123106209+CuiyingLin17@users.noreply.github.com>
2026-05-04 00:09:32 -0500

    Update acquire_cpiaucsl.py

--- notebooks/acquire_cpiaucsl.py
+++ notebooks/acquire_cpiaucsl.py
@@ -54,4 +54,4 @@ print("Date range:", df[df.columns[0]].min(), "to", df[df.columns[0]].max())
 df.head()
 
 os.makedirs("data/filtered_10yrs", exist_ok=True)
-df.to_csv('data/filtered_10yrs/CPIAUCSL_filtered.csv')
+df.to_csv('data/filtered_10yrs/CPIAUCSL_filtered.csv', index = False)
