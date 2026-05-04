6f164180c3fd77b05174e5eb0aa8a2ac2cbc633b
CuiyingLin17 <123106209+CuiyingLin17@users.noreply.github.com>
2026-05-03 23:54:33 -0500

    Add files via upload

---
+++ notebooks/integrate_dataset.py
@@ -0,0 +1,29 @@
+#load datasets
+import pandas as pd
+
+cpi = pd.read_csv('data/filtered_10yrs/CPIAUCSL_filtered.csv')
+income = pd.read_csv('data/filtered_10yrs/DSPI_filtered.csv')
+retail_sales = pd.read_csv('data/filtered_10yrs/RSAFS_filtered.csv')
+unemployment = pd.read_csv('data/filtered_10yrs/UNRATE_filtered.csv')
+
+#merge dataset
+merged_dataset = (
+    cpi.merge(income, on = 'observation_date', how = 'outer')
+    .merge(retail_sales, on = 'observation_date', how = 'outer')
+    .merge(unemployment, on = 'observation_date', how = 'outer')
+)
+
+#check if merged_dataset contains same rows as the datasets
+assert len(merged_dataset) == len(cpi), 'merged_dataset must contain same rows as cpi'
+assert len(merged_dataset) == len(income), 'merged_dataset must contain same rows as income'
+assert len(merged_dataset) == len(retail_sales), 'merged_dataset must contain same rows as retail_sales'
+assert len(merged_dataset) == len(unemployment), 'merged_dataset must contain same rows as unemployment'
+
+#reorder columns
+merged_dataset = merged_dataset[['observation_date', 'UNRATE', 'inflation_rate', 'DSPI', 'RSAFS']]
+#rename columns
+merged_dataset.columns = ['observation_date', 'unemployment_rate', 'inflation_rate', 'disposable_personal_income', 'retail_sales']
+
+#save dataset as csv file
+os.makedirs('results', exist_ok = True)
+merged_dataset.to_csv('results/merged_dataset.csv')
