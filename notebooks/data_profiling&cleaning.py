374647efa3b12e711b6f17495ef3d1006ad5262d
CuiyingLin17 <123106209+CuiyingLin17@users.noreply.github.com>
2026-05-04 00:32:28 -0500

    Add files via upload

---
+++ notebooks/data_profiling&cleaning.py
@@ -0,0 +1,19 @@
+import pandas as pd
+df = pd.read_csv('data/merged_dataset.csv')
+
+#data profiling
+print(df.info())
+print(df[df['unemployment_rate'].isna()])
+print(df[df['inflation_rate'].isna()])
+
+#data cleaning
+df['disposable_personal_income'] = df['disposable_personal_income'] * 1000
+df['retail_sales'] = df['retail_sales'].astype(float)
+
+df = df.rename(columns = {
+    'disposable_personal_income': 'disposable_personal_income_millions',
+    'retail_sales': 'retail_sales_millions'
+})
+
+#save dataset as csv file
+df.to_csv('data/merged_dataset_cleaned.csv', index = False)
