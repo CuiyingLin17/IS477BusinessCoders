8d063e48a78b4283f4ff27f6ac6fe06582349e9c
CuiyingLin17 <123106209+CuiyingLin17@users.noreply.github.com>
2026-05-03 23:51:17 -0500

    Update acquire_dspi.py

--- notebooks/acquire_dspi.py
+++ notebooks/acquire_dspi.py
@@ -14,7 +14,7 @@ import hashlib
 os.makedirs("data/raw", exist_ok=True)
 
 DSPI_url = "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23ebf3fb&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1320&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=DSPI&scale=left&cosd=1959-01-01&coed=2026-01-01&line_color=%230073e6&link_values=false&line_style=solid&mark_type=none&mw=3&lw=3&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2026-04-05&revision_date=2026-04-05&nd=1959-01-01"
-DSPI_filename = "data/raw/DSPI.csv"
+DSPI_filename = "data/raw/DSPI_raw.csv"
 
 response = requests.get(DSPI_url)
 
@@ -51,4 +51,4 @@ print("Date range:", df[df.columns[0]].min(), "to", df[df.columns[0]].max())
 df.head()
 
 os.makedirs("data/filtered_10yrs", exist_ok=True)
-df.to_csv('data/filtered_10yrs/DSPI.csv')
+df.to_csv('data/filtered_10yrs/DSPI_filtered.csv')
