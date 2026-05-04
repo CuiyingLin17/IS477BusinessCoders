5af02dca7ac4d29f1f41520a62dcc7f7bfe30714
CuiyingLin17 <123106209+CuiyingLin17@users.noreply.github.com>
2026-05-03 23:47:23 -0500

    Update acquire_cpiaucsl.py

--- notebooks/acquire_cpiaucsl.py
+++ notebooks/acquire_cpiaucsl.py
@@ -14,7 +14,7 @@ import hashlib
 os.makedirs("data/raw", exist_ok=True)
 
 CPIAUCSL_url = "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23ebf3fb&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1320&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=CPIAUCSL&scale=left&cosd=1947-01-01&coed=2026-02-01&line_color=%230073e6&link_values=false&line_style=solid&mark_type=none&mw=3&lw=3&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2026-04-05&revision_date=2026-04-05&nd=1947-01-01"
-CPIAUCSL_filename = "CPIAUCSL.csv"
+CPIAUCSL_filename = "data/raw/CPIAUCSL.csv"
 
 response = requests.get(CPIAUCSL_url)
 
@@ -53,3 +53,5 @@ df = df.drop(columns=['OLD_CPI'])
 
 df.head()
 
+os.makedirs("data/filtered_10yrs", exist_ok=True)
+df.to_csv('data/filtered_10yrs/CPIAUCSL.csv')
